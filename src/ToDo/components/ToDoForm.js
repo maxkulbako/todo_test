73b1f2075a15e3c5c315e7c52d26acda785aa5bb
max.kulbako <mkulbako@gmail.com>
2023-03-01 15:03:02 +0100

    add base input validation

--- src/ToDo/components/ToDoForm.js
+++ src/ToDo/components/ToDoForm.js
@@ -1,9 +1,72 @@
+import { useState } from 'react';
+
 export function ToDoForm() {
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+
+  const [titleError, setTitleError] = useState(false);
+  const [descriptionError, setDescriptionError] = useState(false);
+  
+  const addItemHandler = (e) => {
+    e.preventDefault();
+    
+    if(!title) {
+      setTitleError(true);
+      return;
+    }
+
+    if(!description) {
+      setDescriptionError(true);
+      return;
+    }
+
+    setTitle('');
+    setDescription('');
+  }
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    switch(name) {
+      case 'title':
+        setTitle(value);
+        setTitleError(false);
+        break;
+      case 'description':
+        setDescription(value);
+        setDescriptionError(false);
+        break;
+      default:
+        break;
+    }
+  }
+
   return (
     <form>
-      <input name="title" placeholder="Enter title"/>
-      <input name="description" placeholder="Enter descriptiom"/>
-      <button>create</button>
+      <div className="form_input_wrapper">
+        <input
+          name='title'
+          style={{ borderColor: titleError ? 'red' : null }}
+          placeholder="Enter title"
+          value={title}
+          onChange={handleInputChange}
+        />
+        {titleError && <p style={{ color: 'red' }}>This field is empty</p>}
+      </div>
+      <div className="form_input_wrapper">
+        <input
+          name='description'
+          style={{ borderColor: descriptionError ? 'red' : null }}
+          placeholder="Enter description"
+          value={description}
+          onChange={handleInputChange}
+        />
+        {descriptionError && <p style={{ color: 'red' }}>This field is empty</p>}
+      </div>
+      <button
+        onClick={addItemHandler}
+      >
+        create
+      </button>
     </form>
   );
 }
